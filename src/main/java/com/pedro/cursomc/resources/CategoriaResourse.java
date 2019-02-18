5309f76b134b54277f49f8729b3dbc135482f75d
Pedrocry <pedrolima634@gmail.com>
2019-02-18 14:31:21 -0300

    REST testado com postman, e JSON

--- src/main/java/com/pedro/cursomc/resources/CategoriaResourse.java
+++ src/main/java/com/pedro/cursomc/resources/CategoriaResourse.java
@@ -1,15 +1,28 @@
 package com.pedro.cursomc.resources;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.pedro.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResourse {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "O REST está funcionando!";
+	public List<Categoria> listar() {
+		Categoria cat=new Categoria(1,"Informatica");
+		Categoria cat2=new Categoria(2,"Escritório");
+		
+		List<Categoria> lista= new ArrayList<>();
+		lista.add(cat);
+		lista.add(cat2);
+		
+		return lista;
+		
 	}
 	
 }
