eff63a315a957504edf683ef96e45db39ef845a7
Pedrocry <pedrolima634@gmail.com>
2019-02-18 13:43:53 -0300

    REST Testado com sucesso, mas com atenção na porta do apache

---
+++ src/main/java/com/pedro/cursomc/resources/CategoriaResourse.java
@@ -0,0 +1,15 @@
+package com.pedro.cursomc.resources;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResourse {
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public String listar() {
+		return "O REST está funcionando!!!";
+	}
+	
+}
